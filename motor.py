295e3a27480a23633121219bb3eb62fa3e9ba114
root <charles-antoine.idrac_github@m4x.org>
2016-03-23 23:23:34 +0100

    first working version

--- motor.py
+++ motor.py
@@ -3,94 +3,11 @@
 from __future__ import print_function
 
 # Import all necessary libraries
-import RPi.GPIO as GPIO, sys, threading, time, os
-#from Adafruit_PWM_Servo_Driver import PWM
-import pca9685_p3 as pca9685
-from sgh_PCF8591P_p3 import sgh_PCF8591P
+import time
+import RPi.GPIO as GPIO
+from board import Board as Board
 
 
-class Board:
-    # Define Type of Pi2Go
-    PGNone = 0
-    PGFull = 1
-    PGLite = 2
-    PGType = PGNone # Set to None until we find out which during init()
-
-    # Pins 24, 26 Left Motor
-    # Pins 19, 21 Right Motor
-    L1 = 26
-    L2 = 24
-    R1 = 19
-    R2 = 21
-
-    def __init__(self):
-        # global p, q, a, b, pwm, pcfADC, PGType
-        # global irFL, irFR, irMID, lineLeft, lineRight
-        GPIO.setwarnings(False)
-
-        self.PGType = self.PGFull
-        # Initialise the PCA9685 PWM device using the default address
-        try:
-            pca9685.init()
-        except:
-            self.PGType = self.PGLite # No PCA9685 so set to Pi2Go-Lite
-
-        # Initalise the ADC
-        self.pcfADC = None # ADC object
-        try:
-            self.pcfADC = sgh_PCF8591P(1) #i2c, 0x48)
-        except:
-            self.PGType = self.PGLite
-
-        #use physical pin numbering
-        GPIO.setmode(GPIO.BOARD)
-
-        # # Define IR sensor pins - varies with model
-        # if PGType == PGLite:
-        #     lineRight = lineRight_2
-        #     lineLeft = lineLeft_2
-        #     irFL = irFL_2
-        #     irFR = irFR_2
-        #     irMID = 0
-        # else:
-        #     lineRight = lineRight_1
-        #     lineLeft = lineLeft_1
-        #     irFL = irFL_1
-        #     irFR = irFR_1
-        #     irMID = irMID_1
-        #
-        # #set up digital line detectors as inputs
-        # GPIO.setup(lineRight, GPIO.IN) # Right line sensor
-        # GPIO.setup(lineLeft, GPIO.IN) # Left line sensor
-        #
-        # #Set up IR obstacle sensors as inputs
-        # GPIO.setup(irFL, GPIO.IN) # Left obstacle sensor
-        # GPIO.setup(irFR, GPIO.IN) # Right obstacle sensor
-        # if PGType == PGFull:
-        #     GPIO.setup(irMID, GPIO.IN) # Centre Front obstacle sensor
-
-
-        # # initialise servos (Pi2Go-Lite only)
-        # if PGType == PGLite:
-        #     startServos()
-        #
-        # #set up Pi2Go-Lite White LEDs as outputs
-        #     GPIO.setup(frontLED, GPIO.OUT)
-        #     GPIO.output(frontLED, 1)    # switch front LEDs off as they come on by default
-        #     GPIO.setup(rearLED, GPIO.OUT)
-        #     GPIO.output(rearLED, 1)    # switch rear LEDs off as they come on by default
-        #
-        # #set switch as input with pullup
-        # if PGType == PGLite:
-        #     GPIO.setup(Lswitch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # else:
-        #     GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        #
-        # # initialise wheel counters if Pi2Go-Lite
-        # if PGType == PGLite:
-        #     threadC = threading.Thread(target = wheelCount)
-        #     threadC.start()
-        #     running = True
 
 
 class Motor:
--- rb.py
+++ rb.py
@@ -2,22 +2,36 @@
 
 from __future__ import print_function
 # Import all necessary libraries
-import time
-# import RPi.GPIO as GPIO, sys, threading, time, os, smbus
-# import pca9685_p3
-# from sgh_PCF8591P_p3 import sgh_PCF8591P
-from motor import Motor
+import time, RPi.GPIO as GPIO
 
+# Robot modules
+from motor import Motor
+from board import Board
+from led import Leds
+from button import Button
+from lcd7segment import Lcd7segment
+from infraRed import InfraRed
+from sonar import Sonar
+
+import os
+def errase_pic(module_name):
+    os.system('rm ' + module_name + '.pyc')
+    
+def reload_modules():
+    for module_name in ['rb', 'motor', 'board', 'led', 'button', 'lcd7segment']:
+        errase_pic(module_name)
 
 class Robot:
     def __init__(self):
-
-        # self.lcd = Lcd7segment()
-        # self.ir = InfraRed()
+        self._board = Board()
         self._speed = 50
         self.motor = Motor(self._speed)
-        # self.button = Button(self)
-        # self.leds = Leds()
+        self.button = Button(self)
+        self.leds = Leds()
+        self.lcd = Lcd7segment()
+        self.ir = InfraRed(self._board)
+        self.sonar = Sonar()
+
         print('Robot is ready !')
 
     # def cleanup(self):
@@ -27,3 +41,33 @@ class Robot:
     def sleep(self, seconds):
         """ shortcut to time.sleep"""
         time.sleep(seconds)
+        
+    def say(self, words):
+        """Scroll some text on the LCD"""
+        self.lcd.scroll(words, 1)
+
+    def getDistance(self):
+        """Return distance with sonar"""
+        GPIO.setup(sonar, GPIO.OUT)
+        # Send 10us pulse to trigger
+        GPIO.output(sonar, True)
+        time.sleep(0.00001)
+        GPIO.output(sonar, False)
+        start = time.time()
+        count=time.time()
+        GPIO.setup(sonar,GPIO.IN)
+        while GPIO.input(sonar)==0 and time.time()-count<0.1:
+            start = time.time()
+        count=time.time()
+        stop=count
+        while GPIO.input(sonar)==1 and time.time()-count<0.1:
+            stop = time.time()
+        # Calculate pulse length
+        elapsed = stop-start
+        # Distance pulse travelled in that time is time
+        # multiplied by the speed of sound 34000(cm/s) divided by 2
+        distance = elapsed * 17000
+        return distance
+
+        
+        
